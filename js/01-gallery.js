2d2bf466ff39fafcafe12ac335bf7a382e69e981
LauraBarrigaDI <113401273+LauraBarrigaDI@users.noreply.github.com>
2023-03-23 09:05:35 -0500

    update1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,27 +1,29 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import "node_modules/simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
-const galleryContainer = document.querySelector(".gallery");
-let galleryMarkup = "";
+const galleryContainer = document.querySelector('.gallery');
+let galleryMarkup = '';
 
 galleryItems.forEach(({ preview, original, description }) => {
   galleryMarkup += `
+  <ul class="gallery">
     <li class="gallery__item">
       <a class="gallery__link" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}" />
       </a>
     </li>
+  </ul>
   `;
 });
 
-galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-const lightbox = new SimpleLightbox(".gallery a", {
+const lightbox = new SimpleLightbox('.gallery a', {
   overlay: true,
   overlayOpacity: 0.7,
   captions: true,
-  captionsData: "alt",
+  captionsData: 'alt',
   captionDelay: 250,
 });
