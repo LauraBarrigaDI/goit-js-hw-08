65718ff0f906ca8e94e907d313fa5590e2b74b3b
LauraBarrigaDI <laurabarriga31@gmail.com>
2023-03-22 22:02:27 -0500

    task3

--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
 
+let formState = {
+  email: '',
+  message: ''
+};
+
+form.addEventListener('input', event => {
+  const input = event.target;
+  formState[input.name] = input.value;
+  saveStateToLocalStorage();
+});
+
+const THROTTLE_DELAY = 500; // 500 milliseconds
+
+const saveStateToLocalStorage = _.throttle(() => {
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, THROTTLE_DELAY);
+
+window.addEventListener('load', () => {
+    const savedState = localStorage.getItem('feedback-form-state');
+    if (savedState) {
+      form
+    };
+});
