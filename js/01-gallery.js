7d1eeaad246926ca9a36d8ece69529f5caf69f0e
LauraBarrigaDI <113401273+LauraBarrigaDI@users.noreply.github.com>
2023-03-23 09:11:29 -0500

    u2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
 // Add imports above this line
-import SimpleLightbox from "simplelightbox";
-import "node_modules/simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
 let galleryMarkup = '';
