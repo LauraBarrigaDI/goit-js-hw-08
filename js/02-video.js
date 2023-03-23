eddb446c82d8c940df3eaa4ba066938fe50686a9
LauraBarrigaDI <113401273+LauraBarrigaDI@users.noreply.github.com>
2023-03-23 15:44:00 -0500

    cambio1

--- js/02-video.js
+++ js/02-video.js
@@ -1,13 +1,14 @@
-import Player from '@vimeo/player'; 
-import { throttle } from 'lodash';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const player = new Player('vimeo-player');
-player.on('timeupdate', throttle(() => { 
-    const currentTime = Math.floor(player.currentTime()); 
-    localStorage.setItem('videoplayer-current-time', currentTime); 
-}, 1000));
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
-const currentTime = localStorage.getItem('videoplayer-current-time'); 
-if (currentTime) { 
-    player.setCurrentTime(currentTime); 
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+  player.setCurrentTime(storedTime);
 }
+
+player.on('timeupdate', throttle(() => {
+  localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
+}, 1000));
