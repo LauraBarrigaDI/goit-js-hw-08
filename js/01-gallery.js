06d57d310e59aed02dd206b93b56d6b658c937e6
LauraBarrigaDI <laurabarriga31@gmail.com>
2023-03-22 22:14:49 -0500

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+let galleryMarkup = "";
 
-console.log(galleryItems);
+galleryItems.forEach(({ preview, original, description }) => {
+  galleryMarkup += `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `;
+});
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  overlay: true,
+  overlayOpacity: 0.7,
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
