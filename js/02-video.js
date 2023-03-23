80d8498120b3465822a9f4e571c8694e2be1dcb8
LauraBarrigaDI <laurabarriga31@gmail.com>
2023-03-22 21:56:05 -0500

    task2

--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player'; 
+import { throttle } from 'lodash';
 
+const player = new Player('vimeo-player');
+player.on('timeupdate', throttle(() => { 
+    const currentTime = Math.floor(player.currentTime()); 
+    localStorage.setItem('videoplayer-current-time', currentTime); 
+}, 1000));
+
+const currentTime = localStorage.getItem('videoplayer-current-time'); 
+if (currentTime) { 
+    player.setCurrentTime(currentTime); 
+}
